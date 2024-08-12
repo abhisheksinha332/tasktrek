b8ac79a78e503aec6dbfc4f8acb817b4ab5437dc
abhisheksinha332 <3327abhishek@gmail.com>
2024-08-13 02:31:36 +0530

    Added functionality to store data in local storage

--- tasktrek/src/App.jsx
+++ tasktrek/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Tasks from "./components/Tasks"
 
 import "./App.css"
 import TaskForm from "./components/TaskForm"
 
 
+const oldTask = localStorage.getItem("tasks")
+console.log(oldTask);
 
 
 function App() {
 
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState(JSON.parse(oldTask) || [])
+
+  useEffect(()=> {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  },[tasks])
 
   const handleDelete = (taskIndex)  => {
     const newTask = tasks.filter((task, index)=>index !== taskIndex)
